cac0d09830aa04d2107aed79a08e12f21d7aa4d0
Zoran Zdravkovic <zoran@mailbox.org>
2020-11-16 17:23:10 +0100

    Initial Commit

---
+++ 2020-11-16--Udder/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  2020-11-16--Udder
+//
+//  Created by Zoran Zdravkovic on 16.11.20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
